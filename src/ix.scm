609d32ffa6348e2c8972db03fd857b6fdae6d54a
alice maz <alice@alicemaz.com>
2020-05-07 17:55:00 -0500

    pontiff new

---
+++ src/ix.scm
@@ -0,0 +1,15 @@
+(module ix ()
+
+(import scheme)
+(import chicken.base)
+(import chicken.type)
+(import chicken.string)
+(import chicken.format)
+
+(define (main)
+  (printf "hello pontiff!\n")
+  (exit 0))
+
+(main)
+
+)
